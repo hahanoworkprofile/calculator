5fe0df7ee745d755a50ae36b809926deaea20664
cloud <terakorn.p2@gmail.com>
2021-12-21 20:50:00 +0700

    clear, delete

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 let value = "";
+let previousValue = ""; 
+let operator = "";
 
 const add = (a, b) => a + b; 
 const subtract = (a, b) => a - b; 
 const multiply = (a, b) => a * b; 
 const divide = (a, b) => a / b; 
-const operate = (operator, a, b) => operator(a, b); 
+const operate = (operator, a, b) => {
+ 
+} 
 
 const currentValue = document.querySelector('.current');
 const updateDisplay = () => currentValue.textContent = value; 
-
-
 
 document.querySelectorAll('[data-number]').forEach(btn => {
     btn.addEventListener('click', () => {
@@ -20,3 +22,25 @@ document.querySelectorAll('[data-number]').forEach(btn => {
     })
 })
 
+document.querySelectorAll('[data-action]').forEach(btn => {
+    btn.addEventListener('click', () => { 
+        atb = btn.getAttribute('data-action');
+
+        switch (atb) {
+            case ("clear"):
+                value = "";
+                previousValue = "";
+                operator = "";
+                updateDisplay();
+                break;
+            case ("equal"):
+                break; 
+            case ("delete"):
+                console.log(value.lengt);
+                value = value.substring(0, value.length - 1); 
+                updateDisplay();
+                break;
+        }
+
+    })
+})
