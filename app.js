6bbf3bb91d6d99926dbb4fd2e1bf4c13d973acf1
cloud <terakorn.p2@gmail.com>
2021-12-21 19:28:51 +0700

    number pads

--- app.js
+++ app.js
@@ -1,38 +1,22 @@
-let calculator = { 
-    currentOperand : '',
-    previousOperand : '',
-    operator: '',
-    
-    operate: (operator, currentOperand, previousOperand) => operator(currentOperand, previousOperand), 
+let value = "";
 
-    add : (a, b) => a + b, 
-    subtract : (a, b) => a - b, 
-    multiply : (a, b) => a * b, 
-    divide : (a,b) => a/b,
-}
+const add = (a, b) => a + b; 
+const subtract = (a, b) => a - b; 
+const multiply = (a, b) => a * b; 
+const divide = (a, b) => a / b; 
+const operate = (operator, a, b) => operator(a, b); 
 
-const previousDisplay = document.querySelector('.previous');
-const currentDisplay = document.querySelector('.current');
+const currentValue = document.querySelector('.current');
+const updateDisplay = () => currentValue.textContent = value; 
 
-document.querySelectorAll('button').forEach(btn => btn.addEventListener('click', e => { 
-    let atb = attribute => btn.getAttribute(attribute);
 
-    if (atb('data-number')) { 
-        calculator.currentOperand += atb('data-number'); 
+
+document.querySelectorAll('[data-number]').forEach(btn => {
+    btn.addEventListener('click', () => {
+        let atb = btn.getAttribute("data-number");
+        if (atb=="." && value.includes('.')) return; 
+        value += atb;
         updateDisplay();
-    }
-    if (atb('data-action')) {
-        if (!calculator.previousOperand) {
-            calculator.previousOperand = calculator.currentOperand + " " + atb('data-action'); 
-            calculator.currentOperand = '';
-            updateDisplay();
-        }
-        // calculator.currentOperand = 
+    })
+})
 
-    }
-}))
-
-function updateDisplay() { 
-    currentDisplay.textContent = calculator.currentOperand;
-    previousDisplay.textContent = calculator.previousOperand;
-}
